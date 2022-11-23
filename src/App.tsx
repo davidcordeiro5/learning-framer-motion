b76a24677f42fb57bcee6d6ef94c2b553abe9a1b
david_c <david.cordeiro@epitech.eu>
2022-11-23 18:11:23 +0100

    [feat] adding basic routing /w eact-router-dom v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Route } from "react-router";
 import "./App.css";
 import Heart from "./components/Heart";
 import Squares from "./components/Squares";
 
 function App() {
   return (
-    <div className="App">
-      <Squares />
-      <Heart />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<>HOME</>} />
+          <Route path="square" element={<Squares />} />
+          <Route path="heart" element={<Heart />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
